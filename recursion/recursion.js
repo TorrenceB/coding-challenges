b901acd286542fd36eaca58315237cd8a82393bd
Torrence Brown <torrenceb90@gmail.com>
2021-11-18 10:55:53 -0700

    Added problem set

--- recursion/recursion.js
+++ recursion/recursion.js
@@ -36,3 +36,60 @@ const printArray = function (arr) {
 
   traverse(0);
 };
+
+/*
+ * 2c. Reverse a string
+ *
+ * Input:   str {String}
+ * Output:  {String}
+ *
+ * Example: reverseString('hello') => 'olleh'
+ */
+
+/*
+  1st: reverse("hello") ==> i === 4, reversedStr === "o";
+  2nd: reverse("hello") ==> i === 3, reversedStr === "ol";
+  3rd: reverse("hello") ==> i === 2, reversedStr === "oll";
+  4th: reverse("hello") ==> i === 1, reversedStr === "olle";
+  5th: reverse("hello") ==> i === 0, reversedStr === "olleh";
+  6th: reverse("hello") ==> i === -1, baseCase met
+*/
+
+const reverseString = function () {};
+
+/* 
+    Given a positive integer n, return an array of all the 
+    binary strings of length n that DO NOT contain consecutive 1s.
+
+    Input: n (Integer)
+    Output: [Str] (Array of Strings)
+
+    Input: 2
+    Output: ["00", "01", "10"]
+
+    Input: 3
+    Output: ["000", "001", "010", "100", "101"]
+
+    Time Complexity: O(2^N)
+    Auxiliary Space Complexity: O(2^N)
+*/
+
+const nonConsecutiveOnes = function (n) {};
+
+/* 
+    A web developer needs to know how to design a web page's size. So, given a specific rectangular web page’s area, 
+    your job by now is to design a rectangular web page, whose length L and width W satisfy the following requirements:
+
+    The area of the rectangular web page you designed must equal to the given target area.
+    The width W should not be larger than the length L, which means L >= W.
+    The difference between length L and width W should be as small as possible.
+    Return an array [L, W] where L and W are the length and width of the web page you designed in sequence.
+
+    Input: area = 4
+    Output: [2,2]
+    Explanation: The target area is 4, and all the possible ways to construct it are [1,4], [2,2], [4,1]. 
+    But according to requirement 2, [1,4] is illegal; according to requirement 3,  [4,1] is not optimal compared to [2,2]. 
+    So the length L is 2, and the width W is 2.
+*/
+
+const buildRectangle = function(area) {}
